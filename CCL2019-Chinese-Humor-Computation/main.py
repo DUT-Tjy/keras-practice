12a197972dbd9a42cc49391654a9ab577144c305
DUT-Tjy <57056813+DUT-Tjy@users.noreply.github.com>
2019-10-30 21:01:31 +0800

    Add files via upload

--- CCL2019-Chinese-Humor-Computation/main.py
+++ CCL2019-Chinese-Humor-Computation/main.py
@@ -1,6 +1,10 @@
+from keras import Sequential
+from keras.layers import Dense, Bidirectional, Embedding, Dropout, LSTM
+from keras.optimizers import Adam
 import preprocessing
 import pandas as pd
-import models.Machine_Learning
+from keras.utils.np_utils import to_categorical
+from word_embedding.Word2Vec import word2vec_weight
 from models.Machine_Learning.TF_IDF import TF_IDF
 from sklearn.linear_model import LogisticRegression
 from sklearn.svm import SVR
@@ -24,13 +28,21 @@ train_sentence = preprocessing.data_preprocessing(train_df['joke'])
 test_sentence = preprocessing.data_preprocessing(test_df['joke'])
 print("Sentence cut is done!")
 
-X = TF_IDF(train_sentence)
-y = train_df['label']
-test = TF_IDF(test_sentence)
+# # TF-IDF对句子进行向量化
+# X = TF_IDF(train_sentence)
+# test = TF_IDF(test_sentence)
+
+"""Word2Vec, 返回词向量矩阵，对训练集测试集进行padding"""
+X, X_test, embedding_matrix, vocab_size = word2vec_weight(train_sentence, test_sentence)
 
 """数据切分为训练集、验证集"""
+# 传统机器学习
+y = train_df['label']
 X_train, X_dev, y_train, y_dev = model_selection.train_test_split(X, y, test_size=0.2)
 print("划分训练集测试集完成！")
+
+"""转换为one-hot编码"""
+y_train = to_categorical(y_train)
 
 # """Logistic Regression"""
 # clf = LogisticRegression(multi_class='ovr', solver='sag', class_weight='balanced')
@@ -44,9 +56,9 @@ print("划分训练集测试集完成！")
 # clf = RandomForestClassifier(n_estimators=15)
 # clf.fit(X_train, y_train)
 
-"""GBDT"""
-clf = GradientBoostingClassifier()
-clf.fit(X_train, y_train)
+# """GBDT"""
+# clf = GradientBoostingClassifier()
+# clf.fit(X_train, y_train)
 
 # """XGBoost"""
 # clf = XGBClassifier()
@@ -84,8 +96,23 @@ clf.fit(X_train, y_train)
 # grid_search_1.fit(X_train, y_train)
 # print(grid_search_1.best_params_, grid_search_1.best_score_)
 
+model = Sequential()
+model.add(Embedding(input_dim=vocab_size + 1,
+                    output_dim=256,
+                    input_length=30,
+                    weights=[embedding_matrix]))
+model.add(Bidirectional(LSTM(64, dropout=0.3, recurrent_dropout=0.1)))
+model.add(Dense(10))
+model.add(Dropout(0.35))
+model.add(Dense(2, activation='softmax'))
+optimizer = Adam(lr=0.005)
+model.compile(loss='categorical_crossentropy', optimizer=optimizer, metrics=['accuracy'])
 
+model.fit(X_train, y_train, batch_size=128, epochs=10)
 
+pred = model.predict(X_dev)
+
+print(f1_score(y_dev, pred))
 
 
 
